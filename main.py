c22c0ab20ce42105d4f3c4843457672de294afff
namheegordonkim <namhee.kim@aalto.fi>
2022-12-19 15:02:41 +0200

    Linear spline actions

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import gym
-import pybullet_envs
 import numpy as np
 import time
 
@@ -15,13 +14,34 @@ def main():
     env = Walker2DBulletEnv(render=True)
     env.reset()
 
-    actions = np.random.random([100, 6])
+    action_pieces = np.random.random([6, 6]) * 0
+    # keypoints
+    action_pieces[0] = np.array([0, 0, 0, 0, 0, 0])  # first keypoint
+    action_pieces[1] = np.array([-0.5, 0, 0, 0, 0, 0])
+    action_pieces[2] = np.array([0, 0, 0, 0, 0, 0])
+    action_pieces[3] = np.array([-1, 0, 0, 0, 0, 0])
+    action_pieces[4] = np.array([0, 0, 0, 0, 0, 0])
+    action_pieces[5] = np.array([0, 0, 0, 0, 0, 0])
+
+    # after interpolation
+    actions = []
+    actions.append(np.linspace(action_pieces[0], action_pieces[1], 50))
+    actions.append(np.linspace(action_pieces[1], action_pieces[2], 50))
+    actions.append(np.linspace(action_pieces[2], action_pieces[3], 50))
+    actions.append(np.linspace(action_pieces[3], action_pieces[4], 50))
+    actions.append(np.linspace(action_pieces[4], action_pieces[5], 50))
+    actions = np.concatenate(actions, axis=0)
+
+    # actions[:, 0] = 1
+    # actions[:, 1] = 0
+    # actions[:, 3] = 0
+    # actions[:, 4] = 0
 
     for a in actions:
         # env.step(np.zeros(6))
         env.step(a)
         env.render()
-        time.sleep(0.033)
+        time.sleep(0.066)
 
     # import pybullet as p
     # import time
