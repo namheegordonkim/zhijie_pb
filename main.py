1e1292456ce45bb7378aa06615ca4df4e26ba194
Zhijie He <zhijiehe97@gmail.com>
2023-01-13 00:29:18 +0200

    fix time

--- main.py
+++ main.py
@@ -17,25 +17,27 @@ import gym
 from envs.envs import Walker2DBulletEnv
 
 import warnings
-warnings.filterwarnings("ignore", category=UserWarning) 
-warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 # Get current dir
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
+
 def main(cfg):
-    h.make_dir(cfg['result_file_path']) # create result folder
-    env = Walker2DBulletEnv(render=False, direction = cfg['direction'])# need to modify later
+    h.make_dir(cfg['result_file_path'])  # create result folder
+    env = Walker2DBulletEnv(render=False, direction=cfg['direction'])  # need to modify later
     # initialize multiple(pop_size) environments
     envs = gym.vector.AsyncVectorEnv(
-        [lambda: Walker2DBulletEnv(render=False, direction = cfg['direction'])] * cfg['pop_size'],
+        [lambda: Walker2DBulletEnv(render=False, direction=cfg['direction'])] * cfg['pop_size'],
         shared_memory=True
-        )
+    )
     # initialize agent
     agent = Agent(env, envs, cfg)
 
     # print cfg
-    print("#"*19 + " Config " + "#"*19)
+    print("#" * 19 + " Config " + "#" * 19)
     print(OmegaConf.to_yaml(cfg))
 
     if cfg['algo'] == "CEM":
@@ -45,35 +47,36 @@ def main(cfg):
         best_reward = cma_es(agent, cfg)
 
     print("\nBest reward: \t", best_reward)
-    
+
     # close the environment
     env.close()
     envs.close()
 
     return best_reward
 
+
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument("--wandb", required=False, help="Open wandb", action='store_true')
-    ap.add_argument("--algo", type=str, required=False, choices=['CEM', 'CMA-ES'], 
-                        help="Choose the algorithm")
-    ap.add_argument("-d", "--direction", type=str, required=False, choices=['forward', 'backward'], 
-                        help="Decide direction")
+    ap.add_argument("--algo", type=str, required=False, choices=['CEM', 'CMA-ES'],
+                    help="Choose the algorithm")
+    ap.add_argument("-d", "--direction", type=str, required=False, choices=['forward', 'backward'],
+                    help="Decide direction")
     ap.add_argument("--iter", type=int, required=False,
-                    help="The number of iterations")      
+                    help="The number of iterations")
     ap.add_argument("--interval", type=int, required=False,
-                    help="The interpolation interval")      
+                    help="The interpolation interval")
     ap.add_argument("--pop_size", type=int, required=False,
-                    help="The size of population ")           
+                    help="The size of population ")
     ap.add_argument("--num_keypoints", type=int, required=False,
-                    help="The number of keypoints ")   
+                    help="The number of keypoints ")
 
-    args = vars(ap.parse_args()) # dict type
+    args = vars(ap.parse_args())  # dict type
     # args = ap.parse_args() Namespace type
-    
-    #------------------------------- Get configuration -----------------------------------------------#
+
+    # ------------------------------- Get configuration -----------------------------------------------#
     # Get config path
-    config_file_path = os.path.join(currentdir ,"cfg/env_cfg.yaml")
+    config_file_path = os.path.join(currentdir, "cfg/env_cfg.yaml")
     # Load config file
     cfg = h.load_config(config_file_path)
 
@@ -122,28 +125,29 @@ if __name__ == '__main__':
     running_time = h.beautify_time(running_time)
 
     print('\nThe time of execution of main multiple process program is: ', running_time)
-    
+
     # save video
     show_video_of_model(False, cfg)
-    
+
     # keep data to wandb
     if cfg['wandb']:
         # Log the video 
         video_name = cfg['env_name'] + "_" + cfg['direction']
         path_to_video = os.path.join(cfg['result_file_path'], "video", "{}.mp4".format(video_name))
-        #wandb.log({"video": wandb.Video(path_to_video, fps=4, format="mp4", caption = video_name)})
+        # wandb.log({"video": wandb.Video(path_to_video, fps=4, format="mp4", caption = video_name)})
         # remove 
         cfg.pop("wandb")
         cfg.pop("result_file_path")
         if cfg["algo"] == "CMA-ES":
             cfg.pop("elite_frac")
         # add wandb table
-        columns = list(cfg.keys()) + ['video', 'best_reward',  'running_time']
+        columns = list(cfg.keys()) + ['video', 'best_reward', 'running_time']
         table = wandb.Table(columns=columns)
-        record = list(cfg.values()) + [wandb.Video(path_to_video, fps=4, format="mp4", caption = video_name), best_reward,  running_time]
-
+        record = list(cfg.values()) + [wandb.Video(path_to_video, fps=4, format="mp4", caption=video_name), best_reward,
+                                       running_time]
+        # add data
         table.add_data(*record)
-        wandb.log({"result_table":table}, commit=False)
+        wandb.log({"result_table": table}, commit=False)
 
         # close wandb
         wandb.finish()
